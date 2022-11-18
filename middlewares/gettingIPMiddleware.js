26f2d8d15fe2f5d6d620d2ecddb3e694b76e0351
gbonilla <bjoseg@gmail.com>
2022-11-18 14:06:06 -0600

    test, req

--- middlewares/gettingIPMiddleware.js
+++ middlewares/gettingIPMiddleware.js
@@ -5,6 +5,9 @@ module.exports = (() => {
 	const { logger } = app.getInstance();
 	return {
 		requestorIPMiddleware: (req, res, next) => {
+			console.log(req.headers)
+			console.log('-----------')
+			console.log(req)
 			logger.debug('getting ip from headers');
 			req.params = {
 				publicIP: "1.2.3.4"
