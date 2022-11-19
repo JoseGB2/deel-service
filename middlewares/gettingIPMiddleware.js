cc2cd5efcdf9105223965d80c57627adf7be5907
gbonilla <bjoseg@gmail.com>
2022-11-18 21:55:04 -0600

    fix, tabs to spaces

--- middlewares/gettingIPMiddleware.js
+++ middlewares/gettingIPMiddleware.js
@@ -3,20 +3,20 @@ const app = require('../utils/AppCore');
 const errorResponse = require('../utils/errorResponse');
 
 module.exports = (() => {
-	const { logger } = app.getInstance();
-	return {
-		requestorIPMiddleware: (req, res, next) => {
-			logger.debug('getting ip from headers');
+    const { logger } = app.getInstance();
+    return {
+        requestorIPMiddleware: (req, res, next) => {
+            logger.debug('getting ip from headers');
 
-			if(!req.headers['x-forwarded-for']){
-				const error = errorResponse.retrieveError('NOT_FOUND');
-				res.status(error.status).send(error.message);
-			} else {
-				req.params = {
-					publicIP: req.headers['x-forwarded-for'].split(',')[0]
-				}
-				next()
-			}
-		}
-	}
+            if(!req.headers['x-forwarded-for']){
+                const error = errorResponse.retrieveError('NOT_FOUND');
+                res.status(error.status).send(error.message);
+            } else {
+                req.params = {
+                    publicIP: req.headers['x-forwarded-for'].split(',')[0]
+                }
+                next()
+            }
+        }
+    }
 })()
