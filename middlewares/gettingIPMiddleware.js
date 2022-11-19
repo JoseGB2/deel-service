c2810607f029ff8b18b2b81fe13e49a5ed838670
JoseGB2 <josebarrios188@gmail.com>
2022-11-18 22:04:39 -0600

    feat, Getting public IP from headers (#10)
    * feat, getting public ip or if not return error
    
    * fix, tabs to spaces
    
    Co-authored-by: gbonilla <bjoseg@gmail.com>

--- middlewares/gettingIPMiddleware.js
+++ middlewares/gettingIPMiddleware.js
@@ -1,15 +1,22 @@
 //const superagent = require('superagent');
 const app = require('../utils/AppCore');
+const errorResponse = require('../utils/errorResponse');
 
 module.exports = (() => {
-	const { logger } = app.getInstance();
-	return {
-		requestorIPMiddleware: (req, res, next) => {
-			logger.debug('getting ip from headers');
-			req.params = {
-				publicIP: "1.2.3.4"
-			}
-			next()
-		}
-	}
+    const { logger } = app.getInstance();
+    return {
+        requestorIPMiddleware: (req, res, next) => {
+            logger.debug('getting ip from headers');
+
+            if(!req.headers['x-forwarded-for']){
+                const error = errorResponse.retrieveError('NOT_FOUND');
+                res.status(error.status).send(error.message);
+            } else {
+                req.params = {
+                    publicIP: req.headers['x-forwarded-for'].split(',')[0]
+                }
+                next()
+            }
+        }
+    }
 })()
