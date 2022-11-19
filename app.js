b2f7988885cffe4a6fe4230fcb12a81779b66876
gbonilla <bjoseg@gmail.com>
2022-11-18 21:55:04 -0600

    fix, tabs to spaces

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const appInstance = require('./utils/AppCore');
 
 appInstance
-	.construct({
-  		appDir: __dirname,
-	}).then((app)=> {
-  		app.server.listen(app.config.api_port || 8080);
-	}).catch((err) => console.log(err));
+    .construct({
+          appDir: __dirname,
+    }).then((app)=> {
+          app.server.listen(app.config.api_port || 8080);
+    }).catch((err) => console.log(err));
